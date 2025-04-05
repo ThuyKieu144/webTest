ff53a58fbc7d5d40eb1e78eb019268eb56d292c0
Thuy_Kieu <163702421+ThuyKieu144@users.noreply.github.com>
2025-04-05 13:40:00 +0700

    Update Program.cs

--- ttxaphuong/Program.cs
+++ ttxaphuong/Program.cs
@@ -48,7 +48,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         new MySqlServerVersion(new Version(8, 0, 2))));
-
+        mySqlOptions => mySqlOptions.EnableRetryOnFailure()
+            
 //Đoạn Services cho chức năng Logic hoạt động
 builder.Services.AddScoped<IAuthService, AuthService>();
 
